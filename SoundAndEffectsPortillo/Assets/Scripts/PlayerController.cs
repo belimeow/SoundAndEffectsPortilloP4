f65642123cfd751ca046727dce3bce9f95597930
belimeow <711592@student.dusd.net>
2023-03-22 11:41:14 -0700

    hi
    I did stuff to my project and made my background loop

--- SoundAndEffectsPortillo/Assets/Scripts/PlayerController.cs
+++ SoundAndEffectsPortillo/Assets/Scripts/PlayerController.cs
@@ -1,24 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        playerRb.AddForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
